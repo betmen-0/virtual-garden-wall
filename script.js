78ff0b055267855e80f2a3006e911feac2ad0031
betmen-0 <mrdevil00003@proton.me>
2026-07-22 18:34:06 +0530

    images & bg color selection added

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const stage = new Konva.Stage({
     height: 480,
 });
 
+const baseURL = "assets/";
+
+const elementsURL = {
+    logo: `${baseURL}logo.png`,
+    monstera: `${baseURL}monstera.svg`,
+    rose: `${baseURL}rose.svg`,
+    ranunculus: `${baseURL}ranuculus.svg`,
+    hibiscus: `${baseURL}hibiscus.svg`,
+    glassRose: `${baseURL}baptism.svg`,
+    sunflowers: `${baseURL}sunflowers.svg`,
+    almond: `${baseURL}almond.svg`,
+    bouquet: `${baseURL}bouquet.svg`,
+};
+
+const layer = new Konva.Layer();
+stage.add(layer);
+
+const background = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: stage.width(),
+    height: stage.height(),
+    fill: "#c5d6e0",
+    strokeWidth: 4,
+});
+layer.add(background);
